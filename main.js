6597fd77d76fa2faf150d61832fb381ddbff9179
Nathan Spotten <spottenn@byui.edu>
2021-02-23 15:26:07 -0700

    week 7 exercises

--- main.js
+++ main.js
@@ -2,34 +2,34 @@ export const weeks = [
     [
         //Week 1
         {
-            label: "Story Editor",
-            path: "exercises_and_notes/week_01/story_editor.html",
+            label: 'Story Editor',
+            path: 'exercises_and_notes/week_01/story_editor.html',
         },
         {
-            label: "Portfolio Exercise",
-            path: "exercises_and_notes/week_01/portfolio_exercise.html",
+            label: 'Portfolio Exercise',
+            path: 'exercises_and_notes/week_01/portfolio_exercise.html',
         }
     ],
     [
         //Week 2
         {
-            label: "Calculator Team Exercise",
-            path: "exercises_and_notes/week_02/calculator_exercise.html",
+            label: 'Calculator Team Exercise',
+            path: 'exercises_and_notes/week_02/calculator_exercise.html',
         },
         {
-            label: "Superhero Quiz",
-            path: "exercises_and_notes/week_02/superhero_quiz_exercise/quiz.html",
+            label: 'Superhero Quiz',
+            path: 'exercises_and_notes/week_02/superhero_quiz_exercise/quiz.html',
         }
     ],
     [
         //Week 3
         {
-            label: "Build Table Exercise",
-            path: "exercises_and_notes/week_03/build_a_table.html",
+            label: 'Build Table Exercise',
+            path: 'exercises_and_notes/week_03/build_a_table.html',
         },
         {
-            label: "Balloon Exercise",
-            path: "exercises_and_notes/week_03/balloon_exercise.html",
+            label: 'Balloon Exercise',
+            path: 'exercises_and_notes/week_03/balloon_exercise.html',
         }
     ],
     [
@@ -38,15 +38,26 @@ export const weeks = [
     [
         //week 5
         {
-            label: "Lint Exercise",
-            path: "exercises_and_notes/week_05/lint_exercise.html",
+            label: 'Lint Exercise',
+            path: 'exercises_and_notes/week_05/lint_exercise.html',
         }
     ],
     [
         //week 6
         {
-            label: "Challenge 1: To-Do SPA",
-            path: "challenges/challenge_1/todo.html",
+            label: 'Challenge 1: To-Do SPA',
+            path: 'challenges/challenge_1/todo.html',
+        }
+    ],
+    [
+        //week 7
+        {
+            label: 'Higher Functions Exercises',
+            path: 'exercises_and_notes/week_07/higher_functions_exercises.html',
+        },
+        {
+            label: 'HTTP and Forms Exercises',
+            path: 'exercises_and_notes/week_07/http_and_forms_exercises.html',
         }
     ]
 ];
